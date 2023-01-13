9a5152dc5e8274aca1c0973fa88066d1557467c6
ANkush Meshram <ankushmeshram@2403@gmail.com>
2023-01-13 15:16:16 +0530

    JS Assignments

---
+++ JS-Assignments/01qstn/readme.md
@@ -0,0 +1,7 @@
+# Variables
+
+code: [Link](variable.js)
+
+output:
+
+![01](https://user-images.githubusercontent.com/118118102/212286857-3e5fcf49-9b05-4e9b-8075-c8bd7f9a3448.png)
---
+++ JS-Assignments/01qstn/variable.js
@@ -0,0 +1,12 @@
+let str = "Hello";
+let isBoolean = true;
+let undef;
+let nulll = null;
+
+console.log(`value: ${str} and type is: ${typeof(str)}`);
+
+console.log(`value: ${isBoolean} and type is: ${typeof(isBoolean)}`);
+
+console.log(`value: ${undef} and type is: ${typeof(undef)}`);
+
+console.log(`value: ${null} and type is: ${typeof(null)}`);
