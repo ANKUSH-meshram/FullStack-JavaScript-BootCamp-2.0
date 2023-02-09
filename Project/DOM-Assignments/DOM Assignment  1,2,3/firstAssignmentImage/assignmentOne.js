77779b50c62b72e952a9a4210458a72894184189
ANkush Meshram <ankushmeshram@2403@gmail.com>
2023-02-09 23:12:13 +0530

    Dom assignmentss

---
+++ Project/DOM-Assignments/DOM Assignment  1,2,3/firstAssignmentImage/assignmentOne.js
@@ -0,0 +1,41 @@
+// Task One
+// let liTags = document.querySelectorAll("li")
+// let liAll = [...liTags]
+// liAll[2].innerText = "Projects"
+
+// const ul = document.querySelector("ul")
+// const textNode = document.createTextNode("Hire Me")
+// ul.appendChild(textNode);
+
+// const logos = document.querySelectorAll("ul");
+// const allLogos = [...logos];
+
+// allLogos[1].remove();
+
+// ------------------------------------------------------
+
+
+// Task 2
+// const btn = document.querySelector("input").placeholder = "Search My Projects"
+
+// --------------------------------------------------------
+
+// Task 3
+// const span = document.querySelectorAll("span")
+// const allSpan = [...span]
+
+// allSpan[2].innerText = "an Employee"
+// allSpan[3].innerText = "iNeuron Intelligence Pvt Ltd"
+
+// ---------------------------------------------------------
+
+// Task 5
+// const newbtn = document.createElement("BUTTON");
+// const node = document.createTextNode("Support Me");
+// newbtn.appendChild(node);
+
+// const element = document.querySelectorAll("div");
+// const allElements = [...element]
+
+// element[4].appendChild(newbtn);
+// ---------------------------------------------------
